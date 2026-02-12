a2578f85f3140cf1b943820e1cbee02c3b3bca26
Vivek <your-email@example.com>
2026-02-12 14:36:08 +0530

    tb added

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ tally = TallyConnector()
 comp = tally.fetch_all_companies()
 for i in comp:
     comp_name = i.get('name')
-    jv = tally.fetch_trial_balance(company_name=comp_name, from_date='20240401', to_date='20240401', debug=True)
-    # trial_balance_to_xlsx(jv, company_name=comp_name, start_date='20240401',end_date='20240401')
+    jv = tally.fetch_trial_balance(company_name=comp_name, from_date='20230401', to_date='20260401', debug=True)
+    trial_balance_to_xlsx(jv, company_name=comp_name, start_date='20240401',end_date='20240401')
